677a07666a6df59ceb7508145ee3fb95e79656ab
gabriela chacon <gabrielachacond@gmail.com>
2020-05-17 14:22:10 -0300

    WorkshopFront

---
+++ script.js
@@ -0,0 +1,10 @@
+// el navedador y la ejecucion
+window.onload = () =>{
+   // alert("hola!") 
+   document.getElementById("switch-modo").onclick = () => {
+     //  alert("click!")
+       document.body.classList.toggle("modo-oscuro")
+   }
+
+
+}
